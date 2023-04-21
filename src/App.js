870714f4ad1774caac7211238d51ef56ed149c67
Tomoyasu Nagata <tomorelo519@gmail.com>
2023-04-21 17:49:42 +0900

    最新版

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Title from "./components/Title";
 import Form from "./components/Form";
 import Results from "./components/Results";
+import Loading from "./components/Loading";
 import { useState } from "react";
 import axios from "axios";
 import './App.css';
 
 function App() {
+  const [loading, setLoading] = useState(false);
   const [city, setCity] = useState("");
   const [results, setResults] = useState({
     country: "",
@@ -16,6 +18,7 @@ function App() {
   });
   const getWeather = async (e) => {
       e.preventDefault();
+      setLoading(true);
       const res = await axios.get(`https://api.weatherapi.com/v1/current.json?key=2accd76d3a8c41398f654929232004&q=${city}&aqi=no`);
       setResults({
         country: res.data.location.country,
@@ -24,13 +27,15 @@ function App() {
         conditionText: res.data.current.condition.text,
         icon: res.data.current.condition.icon,
       });
+      setCity("");
+      setLoading(false);
   }
   return (
     <div className="wrapper">
       <div className="container">
         <Title />
-        <Form setCity={setCity} getWeather={getWeather}/>
-        <Results results={results}/>
+        <Form setCity={setCity} getWeather={getWeather} city={city}/>
+        {loading ? <Loading /> : <Results results={results}/>}
       </div>
     </div>
   ); 
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import axios from "axios";
 
-const Form = ({ setCity, getWeather}) => {
+const Form = ({ city, setCity, getWeather}) => {
     return (
-        <form>
-            <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)}/>
-            <button type="submit" onClick={getWeather}>Get Weather</button>
+        <form onSubmit={getWeather}>
+            <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)} value={city}/>
+            <button type="submit">Get Weather</button>
         </form>
     );
 }
